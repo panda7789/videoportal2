df09eeee9a6a365c830f06d955943259240e5b89
Lukáš Linhart <linhartlukk@gmail.com>
2023-03-22 21:59:07 +0100

    init3

--- backend/Backend/Backend/DbContext.cs
+++ backend/Backend/Backend/DbContext.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
 using System.Reflection.Metadata;
 
 public class MyDbContext : DbContext
 {
-    public DbSet<Blog> Blogs { get; set; }
-    public DbSet<Post> Posts { get; set; }
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<User> Users { get; set; }
 }
---
+++ backend/Backend/Backend/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace Backend.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Initials { get; set; }
+        public Privileges Rights { get; set; }
+}
+
+    public enum Privileges
+    {
+        user,
+        videoEditor,
+        admin
+    }
+
+}
